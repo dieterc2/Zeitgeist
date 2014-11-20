3f675797057344cd8aa429779a6094abc97d6c5b
unknown <dieterc2@EAS-BEN006-A2.it.muohio.edu>
2014-11-20 12:37:12 -0500

    create both header and cpp file for ourTrends

---
+++ ourTrends.h
@@ -0,0 +1,13 @@
+#include "Trends.h"
+
+#include <vector>
+
+class naiveTrends : public Trends {
+public:
+	virtual void increaseCount(std::string s, unsigned int amount);
+	virtual unsigned int getCount(std::string s);
+	virtual std::string getNthPopular(unsigned int n);
+
+protected:
+	std::vector<std::pair<std::string, int> > wordCountVector;
+};
