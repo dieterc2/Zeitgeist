32b454e67bdba9714a2033e23c615be6c2d30f16
clarkdb <clarkdb@miamioh.edu>
2014-11-23 18:51:15 -0500

    Added some outlines for the methods in ourTrends.h.

--- ourTrends.cpp
+++ ourTrends.cpp
@@ -1,1 +1,17 @@
 #include "ourTrends.h"
+
+void increadeCount(std::string s, unsigned int amount){
+
+
+}
+
+
+unsigned int getCount(std::string s){
+
+	return 0;
+}
+
+std::string getNthPopular(unsigned int n){
+
+	return 0;
+}
