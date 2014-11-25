9132d06e6f5665d7b212c706b1a0c652f14ac1cf
Chris Dieter <chrisdieter616@yahoo.com>
2014-11-24 21:39:37 -0500

    work on merge conflicts

--- ourTrends.cpp
+++ ourTrends.cpp
@@ -1,27 +1,61 @@
 #include "ourTrends.h"
+#include <algorithm>
+#include <iostream>
+
+bool compareFunc(std::pair<std::string, unsigned int> i, std::pair<std::string, unsigned int> j);
 
 void ourTrends::increaseCount(std::string s, unsigned int amount){
 	// check to see if key is already in
-	if(wordStoreTable->find(s) == wordStoreTable->end()){
+	if(wordStoreTable.find(s) == wordStoreTable.end()){
 		// add the element into the hash table
-		std::pair<std::string, int> word (s, amount);
-		wordStoreTable->insert(word);
+		std::vector<std::pair<std::string, int>>::iterator it;
+		std::pair<std::string, std::vector<std::pair<std::string, int>>::iterator> word (s, it);
+		wordStoreTable.insert(word);
+
+		// add element to back of vector
+		std::pair<std::string, int> p1 (s, amount);
+		sortedVector.push_back(p1);
+		std::swap(sortedVector.front(), sortedVector.back());
+		// set the iterator of the hash pair to the end of the current vector size
+		it = sortedVector.begin();
+		wordStoreTable.find(s)->second = it;
+		isSorted = false;
+		
 	} else{
-		// just update the count
-		wordStoreTable->find(s)->second++;
+		int x = wordStoreTable.find(s)->second->second;
+		std::pair<std::string, int> p1 (s, x + amount);
+		sortedVector.erase(wordStoreTable.find(s)->second);
+		sortedVector.push_back(p1);
+		
+		std::vector<std::pair<std::string, int>>::iterator it = sortedVector.begin();
+
+		wordStoreTable.find(s)->second = it;
+		
+
 	}
 }
 
 unsigned int ourTrends::getCount(std::string s){
-	unsigned int x = wordStoreTable->find(s)->second;
-	return x;
+	std::vector<std::pair<std::string, int>>::iterator it = wordStoreTable.find(s)->second;
+	return it->second;
+	
 }
 
 std::string ourTrends::getNthPopular(unsigned int n){
-	std::string x = NULL;
-	return x;
+	if (!isSorted){
+		std::sort(sortedVector.begin(), sortedVector.end(), compareFunc);
+		isSorted = true;
+	}
+
+	if (n <= numEntries()){
+		return sortedVector.at(n).first;
+	}
+
+	//If they give bad input, return empty string.
+	return "";
 }
 
 unsigned int ourTrends::numEntries() {
-	return wordStoreTable->size();
+	int x = sortedVector.size();
+	return wordStoreTable.size();
 }
--- ourTrends.h
+++ ourTrends.h
@@ -5,11 +5,14 @@
 
 class ourTrends : public Trends {
 public:
+	ourTrends() {isSorted = true;}
 	virtual void increaseCount(std::string s, unsigned int amount);
 	virtual unsigned int getCount(std::string s);
 	virtual std::string getNthPopular(unsigned int n);
 	virtual unsigned int numEntries();
 
 protected:
-	std::unordered_map<std::string, int>* wordStoreTable;
+	std::unordered_map<std::string, std::vector<std::pair<std::string, int>>::iterator> wordStoreTable;
+	std::vector<std::pair<std::string, int>> sortedVector;
+	bool isSorted;
 };
