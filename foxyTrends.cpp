a86572d8b5cabdccf4a888494ffd6ff4a7353560
Chris Dieter <chrisdieter616@yahoo.com>
2014-11-26 16:33:24 -0500

    Final implementation of our trends, renamed our classes to foxyTrends as well to differentiate them from others

---
+++ foxyTrends.cpp
@@ -0,0 +1,56 @@
+#include "ourTrends.h"
+#include <algorithm>
+#include <iostream>
+
+bool compareFunc(std::pair<std::string, unsigned int> i, std::pair<std::string, unsigned int> j);
+
+void ourTrends::increaseCount(std::string s, unsigned int amount){
+	// check to see if key is already in
+	if(wordStoreTable.find(s) == wordStoreTable.end()){
+		// add the element into the hash table mapped to array
+		std::pair<std::string, int> word (s, count);
+		wordStoreTable.insert(word);
+
+		// add element to hash table to keep track of count
+		std::pair<std::string, int> word1 (s, amount);
+		countStoreTable.insert(word1);
+
+		// add element to back of vector
+		std::pair<std::string, int> p1 (s, amount);
+		sortedArray[count] = p1;
+
+		// set the iterator of the hash pair to the end of the current vector size
+		isSorted = false;
+		count++;
+		
+	} else{
+		int y = wordStoreTable.find(s)->second;
+		sortedArray[y].second++;
+
+		countStoreTable.find(s)->second++;
+	}
+}
+
+unsigned int ourTrends::getCount(std::string s){
+	return countStoreTable.find(s)->second;
+	
+}
+
+std::string ourTrends::getNthPopular(unsigned int n){
+	if (!isSorted){
+		std::sort(sortedArray.begin(), sortedArray.end(), compareFunc);
+		isSorted = true;
+	}
+
+	if (n <= numEntries()){
+		return sortedArray[n].first;
+	}
+
+	//If they give bad input, return empty string.
+	return "";
+	return "end";
+}
+
+unsigned int ourTrends::numEntries() {
+	return wordStoreTable.size();
+}
